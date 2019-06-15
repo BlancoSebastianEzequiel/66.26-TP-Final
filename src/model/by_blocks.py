79f800590fc44f86c26cc3169564130c46f0822a
sebastian <sebastian.e.blanco@gmail.com>
2019-06-15 01:02:01 -0300

    use utils sum function to sum matrices in reduce function. Change the partition by getting a matrix of partitions.

--- src/model/by_blocks.py
+++ src/model/by_blocks.py
@@ -1,6 +1,6 @@
 import numpy as np
 from src.model.multiply_matrices_interface import MultiplyMatricesInterface
-from src.controller.utils import get_partitions
+from src.controller.utils import get_partitions, sum_matrices
 
 
 class ByBlocks(MultiplyMatricesInterface):
@@ -12,38 +12,27 @@ class ByBlocks(MultiplyMatricesInterface):
         col_p = kwargs.get('col_p', 2)
         blocks_a = get_partitions(matrix_a, row_p, col_p)
         blocks_b = get_partitions(matrix_b, row_p, col_p)
-        for idx, elem_a in enumerate(blocks_a):
-            r, c, block = elem_a
-            row_blocks = []
-            for elem_b in blocks_b:
-                r_b, c_b, a_block_b = elem_b
-                if c == r_b:
-                    row_blocks.append((a_block_b, c_b))
-            output.append((r, c, block, row_blocks))
+        for r_a in range(0, row_p):
+            for c_a in range(0, col_p):
+                a_block = blocks_a[r_a][c_a]
+                output.append((r_a, a_block, blocks_b[c_a]))
         return output
 
     @staticmethod
     def map_worker(chunk):
-        r, c, block_a, blocks_b = chunk
+        r_a, block_a, blocks_b = chunk
         output = []
-        for elem in blocks_b:
-            block, c_b = elem
-            result = np.matmul(block_a, block).tolist()
-            key = (r, c_b)
+        col_size = len(blocks_b)
+        for c_b in range(0, col_size):
+            result = np.matmul(block_a, blocks_b[c_b]).tolist()
+            key = (r_a, c_b)
             output.append((key, result))
         return output
 
     @staticmethod
     def reduce_worker(item):
         output_pos, values = item
-        result = []
-        size = len(values)
-        for idx in range(0, size-1):
-            partial_add = np.add(values[idx], values[idx+1]).tolist()
-            if not result:
-                result = partial_add
-            else:
-                result = np.add(partial_add, result.copy()).tolist()
+        result = sum_matrices(values)
         output = []
         row_size = len(result)
         block_pos_i, block_pos_j = output_pos
