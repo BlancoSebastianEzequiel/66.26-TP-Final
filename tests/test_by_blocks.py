0c96cbd207dbe8185d43cb0a180a4a7f568cc1c5
sebastian <sebastian.e.blanco@gmail.com>
2019-06-18 21:39:52 -0300

    add more test with different number or workers

--- tests/test_by_blocks.py
+++ tests/test_by_blocks.py
@@ -6,6 +6,18 @@ from src.controller.utils import get_random_matrix_of_dim_n
 def test_simple_multiplication():
     matrix_a = get_random_matrix_of_dim_n(4)
     matrix_b = get_random_matrix_of_dim_n(4)
+    multiply_matrices(ByBlocks, matrix_a, matrix_b, 1)
+
+    matrix_a = get_random_matrix_of_dim_n(4)
+    matrix_b = get_random_matrix_of_dim_n(4)
+    multiply_matrices(ByBlocks, matrix_a, matrix_b, 2)
+
+    matrix_a = get_random_matrix_of_dim_n(4)
+    matrix_b = get_random_matrix_of_dim_n(4)
+    multiply_matrices(ByBlocks, matrix_a, matrix_b, 3)
+
+    matrix_a = get_random_matrix_of_dim_n(4)
+    matrix_b = get_random_matrix_of_dim_n(4)
     multiply_matrices(ByBlocks, matrix_a, matrix_b, 4)
 
     matrix_a = get_random_matrix_of_dim_n(4)
--- tests/test_element_by_row_block.py
+++ tests/test_element_by_row_block.py
@@ -6,6 +6,10 @@ from src.controller.utils import get_random_matrix_of_dim_n
 def test_simple_multiplication():
     matrix_a = get_random_matrix_of_dim_n(4)
     matrix_b = get_random_matrix_of_dim_n(4)
+    multiply_matrices(ElementByRowBlock, matrix_a, matrix_b, 1)
+
+    matrix_a = get_random_matrix_of_dim_n(4)
+    matrix_b = get_random_matrix_of_dim_n(4)
     multiply_matrices(ElementByRowBlock, matrix_a, matrix_b, 4)
 
     matrix_a = get_random_matrix_of_dim_n(64)
