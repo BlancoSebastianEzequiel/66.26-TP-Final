5471f33d159c8d7815926844367f591f7060759b
sebastian <sebastian.e.blanco@gmail.com>
2019-06-20 23:15:02 -0300

    add function to count number of folders

--- src/controller/utils.py
+++ src/controller/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 from math import ceil
 
 
@@ -94,3 +95,11 @@ def chunks(a_list, num):
         out.append(a_list[int(last):int(last + avg)])
         last += avg
     return out
+
+
+def get_version_number():
+    folders = 0
+    for _, dir_names, _ in os.walk('docs/report/pics/'):
+        folders += len(dir_names)
+    return folders
+
