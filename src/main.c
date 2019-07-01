1b762df2296c0f849722c59f1e08d1534e1cb56e
sebastian <sebastian.e.blanco@gmail.com>
2019-07-01 01:10:37 -0300

    multiply matrices from less than 500x500 to avoid valgrind error and use file struct to dump results

--- src/main.c
+++ src/main.c
@@ -4,13 +4,10 @@
 #include "cblas/cblas_dgemm.h"
 #include "controller/utils.h"
 #include "vectorization/blocked_dgemm_sse.h"
+#include "controller/file.h"
 
-int run_cblas_dgemm() {
+double run_cblas_dgemm(int N, double* A, double* B, double* C) {
     clock_t start, stop;
-    int N = 500;
-    double A[N*N];
-    double B[N*N];
-    double C[N*N];
     double alpha = 1.0;
     double beta = 0.0;
     init_arr(N, N, 2, A);
@@ -21,15 +18,11 @@ int run_cblas_dgemm() {
     stop = clock();
     double elapsed_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
     printf("Elapsed time = %lf seconds\n", elapsed_seconds);
-    return 0;
+    return elapsed_seconds;
 }
 
-int run_blocked_dgemm_sse() {
+double run_blocked_dgemm_sse(int N, double* A, double* B, double* C) {
     clock_t start, stop;
-    int N = 500;
-    double A[N*N];
-    double B[N*N];
-    double C[N*N];
     init_arr(N, N, 2, A);
     init_arr(N, N, 1, B);
     init_arr(N, N, 0, C);
@@ -38,11 +31,36 @@ int run_blocked_dgemm_sse() {
     stop = clock();
     double elapsed_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
     printf("Elapsed time = %lf seconds\n", elapsed_seconds);
-    return 0;
+    return elapsed_seconds;
 }
 
 int main() {
-    run_cblas_dgemm();
-    run_blocked_dgemm_sse();
+    char* attributes[4] = {
+            "program",
+            "time_elapsed",
+            "matrix_dim",
+            "number_of_threads"
+    };
+    file_t* file = create_file("src/data/dgemm.csv", "w+", attributes, 4);
+    int N = 400;
+    double A[N*N];
+    double B[N*N];
+    double C[N*N];
+    double elapsed_seconds;
+
+
+    char elapsed_second_str[20];
+    char* values[4] = {"cblas_dgemm", elapsed_second_str, "500", "1"};
+
+    elapsed_seconds = run_cblas_dgemm(N, A, B, C);
+    double_to_string(elapsed_second_str, elapsed_seconds, 20);
+    add_row(file, values, 4);
+
+    elapsed_seconds = run_blocked_dgemm_sse(N, A, B, C);
+    double_to_string(elapsed_second_str, elapsed_seconds, 20);
+    values[0] = "blocked_dgemm_sse";
+    values[1] = elapsed_second_str;
+    add_row(file, values, 4);
+    delete(file);
     return 0;
 }
