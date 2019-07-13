3b4d4ec5cb6c708834215337902eb783081ce02c
sebastian <sebastian.e.blanco@gmail.com>
2019-07-13 14:08:14 -0300

    removing unused import an d version number

--- src/graphs.py
+++ src/graphs.py
@@ -1,10 +1,9 @@
 from src.controller.generate_output_data import OutputData
-from src.controller.utils import get_version_number
 
-output = OutputData(get_version_number)
+output = OutputData()
 output.read_dfs_data_from_json()
 output.graph_dfs()
 
-dgemm_output_data = OutputData(get_version_number, avoid=True)
+dgemm_output_data = OutputData()
 dgemm_df = dgemm_output_data.get_df_from_csv("src/data/dgemm.csv")
 dgemm_output_data.save_df_in_image(dgemm_df, "dgemm.png")
