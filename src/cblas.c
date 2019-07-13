111db276ca44d617296d40cf65e0ed74319d41f4
sebastian <sebastian.e.blanco@gmail.com>
2019-07-13 14:09:30 -0300

    separate cblas and mmx in two separate files becauses they have different compilation flags

--- src/cblas.c
+++ src/cblas.c
@@ -3,7 +3,6 @@
 #include <time.h>
 #include "cblas/cblas_dgemm.h"
 #include "controller/utils.h"
-#include "vectorization/blocked_dgemm_sse.h"
 #include "controller/file.h"
 
 double run_cblas_dgemm(int N, double* A, double* B, double* C) {
@@ -15,19 +14,6 @@ double run_cblas_dgemm(int N, double* A, double* B, double* C) {
     init_arr(N, N, 0, C);
     start = clock();
     mult(A, B, C, alpha, beta, N, N, N);
-    stop = clock();
-    double elapsed_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
-    printf("Elapsed time = %lf seconds\n", elapsed_seconds);
-    return elapsed_seconds;
-}
-
-double run_blocked_dgemm_sse(int N, double* A, double* B, double* C) {
-    clock_t start, stop;
-    init_arr(N, N, 2, A);
-    init_arr(N, N, 1, B);
-    init_arr(N, N, 0, C);
-    start = clock();
-    square_dgemm_blocked_sse(A, B, C, N, 2);
     stop = clock();
     double elapsed_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
     printf("Elapsed time = %lf seconds\n", elapsed_seconds);
@@ -47,19 +33,10 @@ int main() {
     double B[N*N];
     double C[N*N];
     double elapsed_seconds;
-
-
     char elapsed_second_str[20];
-    char* values[4] = {"cblas_dgemm", elapsed_second_str, "500", "1"};
-
+    char* values[4] = {"cblas_dgemm", elapsed_second_str, "400", "1"};
     elapsed_seconds = run_cblas_dgemm(N, A, B, C);
     double_to_string(elapsed_second_str, elapsed_seconds, 20);
-    add_row(file, values, 4);
-
-    elapsed_seconds = run_blocked_dgemm_sse(N, A, B, C);
-    double_to_string(elapsed_second_str, elapsed_seconds, 20);
-    values[0] = "blocked_dgemm_sse";
-    values[1] = elapsed_second_str;
     add_row(file, values, 4);
     delete(file);
     return 0;
---
+++ src/mmx.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "controller/utils.h"
+#include "vectorization/blocked_dgemm_sse.h"
+#include "controller/file.h"
+
+double run_blocked_dgemm_sse(int N, double* A, double* B, double* C) {
+    clock_t start, stop;
+    init_arr(N, N, 2, A);
+    init_arr(N, N, 1, B);
+    init_arr(N, N, 0, C);
+    start = clock();
+    square_dgemm_blocked_sse(A, B, C, N, 2);
+    stop = clock();
+    double elapsed_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
+    printf("Elapsed time = %lf seconds\n", elapsed_seconds);
+    return elapsed_seconds;
+}
+
+int main() {
+    char* attributes[4] = {
+            "program",
+            "time_elapsed",
+            "matrix_dim",
+            "number_of_threads"
+    };
+    file_t* file = create_file("src/data/dgemm.csv", "a", attributes, 4);
+    int N = 400;
+    double A[N*N];
+    double B[N*N];
+    double C[N*N];
+    double elapsed_seconds;
+    char elapsed_second_str[20];
+    char* values[4] = {"blocked_dgemm_sse", elapsed_second_str, "400", "1"};
+    elapsed_seconds = run_blocked_dgemm_sse(N, A, B, C);
+    double_to_string(elapsed_second_str, elapsed_seconds, 20);
+    add_row(file, values, 4);
+    delete(file);
+    return 0;
+}
