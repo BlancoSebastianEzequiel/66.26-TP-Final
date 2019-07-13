c66f2168710856737b8c02c244c822a2ff121223
sebastian <sebastian.e.blanco@gmail.com>
2019-07-13 17:51:46 -0300

    dump elapsed time to csv

--- src/test_pool.c
+++ src/test_pool.c
@@ -1,18 +1,33 @@
 #include <time.h>
 #include <stdio.h>
+#include "controller/file.h"
+#include "controller/utils.h"
 
-#define SIZE 1000000000
-// #define SIZE 1000000
+#define SIZE 10000000
 
 int main() {
     clock_t start, stop;
+    start = clock();
     unsigned long sum = 0;
     for (unsigned long i = 0; i < SIZE; i++) {
         sum += i;
     }
-    start = clock();
     stop = clock();
+    char* attributes[4] = {
+            "program",
+            "time_elapsed (sec)",
+            "iterations",
+            "number_of_threads"
+    };
+    file_t* file = create_file("src/data/test_pool.csv", "w+", attributes, 4);
     double elapsed_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
     printf("Elapsed time = %lf seconds\n", elapsed_seconds);
+
+    char elapsed_second_str[20];
+    char size_str[20];
+    double_to_string(elapsed_second_str, elapsed_seconds, 20);
+    int_to_string(size_str, SIZE, 20);
+    char* values[4] = {"c program", elapsed_second_str, size_str, "1"};
+    add_row(file, values, 4);
     return 0;
 }
--- src/test_pool.py
+++ src/test_pool.py
@@ -2,17 +2,24 @@ from multiprocessing import Pool
 from time import time
 from math import ceil
 from src.controller.utils import chunks
+from src.controller.generate_output_data import OutputData
+import pandas as pd
+
+SIZE = 10000000
 
 
 def loop(s):
-    return sum(s)
+    acum = 0
+    size = len(s)
+    for i in range(0, size):
+        acum += s[i]
+    return acum
 
 
-size = 100000000
-a_list = list(range(size))
+a_list = list(range(SIZE))
 pool = Pool(processes=4)
 start = time()
-pool_sum = pool.map(loop, chunks(a_list, 4), chunksize=ceil(size/4))
+pool_sum = pool.map(loop, chunks(a_list, 4), chunksize=ceil(SIZE/4))
 pool.close()
 pool.join()
 end = time()
@@ -25,3 +32,17 @@ ratio = pool_elapsed_time/elapsed_time
 print(f"pool_elapsed_time: {pool_elapsed_time}")
 print(f"elapsed_time: {elapsed_time}")
 print(f"ratio: {ratio}")
+
+test_pool_output_data = OutputData()
+test_pool_df = test_pool_output_data.get_df_from_csv("src/data/test_pool.csv")
+test_pool_df = test_pool_df.append(
+    pd.Series(
+        ["python", str(elapsed_time), str(float(SIZE)), "1"],
+        index=test_pool_df.columns),
+    ignore_index=True)
+test_pool_df = test_pool_df.append(
+    pd.Series(
+        ["python using pool", str(pool_elapsed_time), str(float(SIZE)), "4"],
+        index=test_pool_df.columns),
+    ignore_index=True)
+test_pool_output_data.save_df_in_image(test_pool_df, "test_pool.png")
